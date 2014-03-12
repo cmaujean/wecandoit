1d9af76d9193b698f5c6fb39b9291ec8e23f24ee
Hongxu Liu <djshu.us@gmail.com>
2014-03-12 08:13:17 -0700

    Added a strip view in menu view

--- js/app/MenuView.js
+++ js/app/MenuView.js
@@ -4,14 +4,27 @@ define(function(require, exports, module) {
     var Transform       = require('famous/core/Transform');
     var View            = require('famous/core/View');
 
+    var StripView       = require('./StripView');
+
     function MenuView() {
         View.apply(this, arguments);
+
+        _createStripViews.call(this);
     }
 
     MenuView.prototype = Object.create(View.prototype);
     MenuView.prototype.constructor = MenuView;
 
     MenuView.DEFAULT_OPTIONS = {};
+
+    function _createStripViews() {
+        var stripView = new StripView();
+        var stripModifier = new Modifier({
+            transform: Transform.translate(0, 200, 0)
+        });
+
+        this._add(stripModifier).add(stripView);
+    }
 
     module.exports = MenuView;
 });
